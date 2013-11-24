7e31933e851f2c118abbb54406d757d2871ac517
Sebastian Hermida <sebastian@path11.com>
2013-11-24 15:35:07 +0100

    Updated the time markup with generic elements and without the slash

--- audiojs/audio.js
+++ audiojs/audio.js
@@ -68,7 +68,7 @@
             <div class="loaded"></div> \
           </div> \
           <div class="time"> \
-            <em class="played">00:00</em>/<strong class="duration">00:00</strong> \
+            <span class="played">00:00</span><span class="duration">00:00</span> \
           </div> \
           <div class="error-message"></div>',
         playPauseClass: 'play-pause',
